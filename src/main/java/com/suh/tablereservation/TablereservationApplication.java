aeda182dca18421bf747aafad3fad74f836cd56b
서새찬 <chan4760@gmail.com>
2023-12-27 18:40:24 +0900

    Init : [001-INIT] : 기본 파일

---
+++ src/main/java/com/suh/tablereservation/TablereservationApplication.java
@@ -0,0 +1,13 @@
+package com.suh.tablereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablereservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TablereservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suh/tablereservation/TablereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suh.tablereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablereservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
